829c63039e00841b8f455c668ef1cd399407429c
kalpit10 <kalpit.swami@gmail.com>
2024-02-16 17:06:17 +0530

    Q5

--- StringIsPalindrome.js
+++ StringIsPalindrome.js
@@ -2,6 +2,7 @@
 // For eg:- "Naman" -> "Naman"
 // Output should return true or false.
 
+//T.C = O(n/2) , S.C = O(n/2)  as we are checking the half list only
 function reverseStr(str, start) {
   if (start > Math.floor(str.length / 2)) return true;
 
@@ -10,5 +11,5 @@ function reverseStr(str, start) {
   return reverseStr(str, start + 1);
 }
 
-let str = "121!1";
+let str = "12121";
 console.log(reverseStr(str, 0));
