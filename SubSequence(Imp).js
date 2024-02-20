06efb96c0ba7e6e5c55cf8bef315c6d6e3d563ac
kalpit10 <kalpit.swami@gmail.com>
2024-02-20 16:11:24 +0530

    Q7 Important

--- SubSequence(Imp).js
+++ SubSequence(Imp).js
@@ -3,6 +3,10 @@
 // Here we notice that all the pairs can be made in order only, that means i cannot make [2,1] or [3,2,1] as it does not fall in the order of original array [3,1,2]
 // Refer striver lecture 6 of recursion for better understanding
 
+// T.C = O(2^n),
+// S.C = O(n) => The space occupied by this array grows as the recursion depth increases. However, at any given point during the execution of the code,
+// the maximum size of the ds array is bounded by the length of the input array arr, which is 'n'.
+// This is because for each element in the input array, there are two recursive calls: one where the current element is included in the subsequence, and one where it is not included.
 function printF(ind, ds, arr, n) {
   if (ind === n) {
     if (ds.length === 0) {
